b40662117acd851c7bf9a1d4ebe5939a545d82a6
Raul Luis <soyrauluis@outlook.com>
2022-01-19 23:31:38 -0400

    init

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+namespace Capitulos
+{
+
+    public class Program
+    {
+        public static void Main()
+        {
+
+            // Capitulo1.Ejercicio1();
+
+            // Capitulo2.Ejercicio1();
+            // Capitulo2.Ejercicio3();
+            // Capitulo2.Ejercicio4();
+            // Capitulo2.Ejercicio5();
+            // Capitulo3.Ejercicio1();
+            // Capitulo3.Ejercicio5();
+        }
+
+
+
+    }
+
+
+}
